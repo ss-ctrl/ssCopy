4be6aa672cc36f184fdd00b6d7a262237e90ccb5
Sun <1654341647@qq.com>
2022-04-22 14:27:55 +0800

    小更新：自选的路径做优化

--- js/fastCopy.js
+++ js/fastCopy.js
@@ -1,20 +1,17 @@
 const { execFile } = require('child_process')
-const { SS, Document} = require('../ss.config')
-
-
+const { SS, Document } = require('../ss.config')
 
 /**
-   * @author V
-   * @description 调用FastCopy，将onedrive中的Note/Config文件夹备份到U盘
-   * @param {Array} RemovableDisk 移动磁盘数组 每个磁盘是一个对象
-   * @return void
-   * @time 2022-04-21 16:12:55
-   */
- function fastCopy_sync_config_note_ToMoveDisk(RemovableDisk) {
+ * @author V
+ * @description 调用FastCopy，将onedrive中的Note/Config文件夹备份到U盘
+ * @param {Array} RemovableDisk 移动磁盘数组 每个磁盘是一个对象
+ * @return void
+ * @time 2022-04-21 16:12:55
+ */
+function fastCopy_sync_config_note_ToMoveDisk(RemovableDisk) {
   RemovableDisk.forEach((v) => {
     console.log(`找到的磁盘移动磁盘:${v.mounted}`)
   })
-  // return
   console.log('config/note拷贝到' + RemovableDisk[0].mounted)
   execFile(
     'FastCopy.exe',
@@ -39,13 +36,13 @@ const { SS, Document} = require('../ss.config')
 }
 
 /**
-   * @author V
-   * @description 调用FastCopy，复制vscode的用户代码片段/键盘映射/json设置等至文档中的onedrive文件夹(onedrive可以上传至云)
-   * @param {*} null
-   * @return void
-   * @time 2022-04-21 16:12:55
-   */
- function fastCopy_sync_vscodeSettingsToOneDrive() {
+ * @author V
+ * @description 调用FastCopy，复制vscode的用户代码片段/键盘映射/json设置等至文档中的onedrive文件夹(onedrive可以上传至云)
+ * @param {*} null
+ * @return void
+ * @time 2022-04-21 16:12:55
+ */
+function fastCopy_sync_vscodeSettingsToOneDrive() {
   execFile(
     'FastCopy.exe',
     [
@@ -69,7 +66,6 @@ const { SS, Document} = require('../ss.config')
   )
 }
 
-
 /**
  * @author V
  * @description 调用FastCopy，将onedrive中的Note/Config文件夹备份到某个磁盘（自选）
@@ -78,9 +74,24 @@ const { SS, Document} = require('../ss.config')
  * @time 2022-04-21 16:12:55
  */
 function fastCopy_sync_config_note_ToChooseDisk(disk) {
+  // XXX 对路径做个简单处理
   // console.log(disk.length);
-  if (disk.length === 1) disk = disk + ':'
-  console.log('config/note拷贝到' + disk);
+  if (disk.length === 1) disk = disk + ':\\'
+  else if (disk.length > 1) {
+    if (disk.includes('/')) {
+      // 用于windows， 把所有的/改为\
+      disk = disk.replace(new RegExp('/', 'gm'), '\\')
+    }
+    if (!disk.includes(':')) {
+      // 如果没有':' 则在第一个字母上加上':'
+      // console.log(disk.length);
+      const driveLetter = disk.substring(0, 1)
+      const drivePath = disk.substring(1, disk.length)
+      disk = driveLetter + ':' + drivePath
+    }
+  }
+  console.log('config/note拷贝到' + disk)
+  // return
   execFile(
     'FastCopy.exe',
     [
@@ -101,10 +112,10 @@ function fastCopy_sync_config_note_ToChooseDisk(disk) {
       process.exit()
     }
   )
- }
+}
 
- module.exports = {
+module.exports = {
   fastCopy_sync_vscodeSettingsToOneDrive,
   fastCopy_sync_config_note_ToMoveDisk,
-  fastCopy_sync_config_note_ToChooseDisk,
- }
+  fastCopy_sync_config_note_ToChooseDisk
+}
